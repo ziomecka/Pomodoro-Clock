3ef7f6d83381bfe64b5768b71f13da79d88611dd
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-03 10:45:57 +0100

    Comments changed (for Jsdoc)

--- app/break/timer.break.component.js
+++ app/break/timer.break.component.js
@@ -1,4 +1,10 @@
 /* jshint esversion: 6 */
+/**
+ * @module      break/timer.break.component
+ * @name        break/timer.break.component.breakComponent
+ * @requires    {@link break/timer.break.controller.js BreakController}
+ * @description Break component's configuration object
+ */
 import controller from "./timer.break.controller";
 import template from "../core/timer.template.html";
 
