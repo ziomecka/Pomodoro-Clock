607546f48b4a3b3ba186fc68960e5bb1c73dfba5
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-03 10:44:37 +0100

    Comments added (for Jsdoc and custom template) Import of index.hmtl moved to index.js

--- app/app.module.js
+++ app/app.module.js
@@ -1,12 +1,26 @@
 /* jshint esversion: 6 */
+/**
+ *  @module       app.module
+ *  @name         app.module.AppModule
+ *  @requires     {@link app.controller.js AppController}
+ *  @requires     {@link work/timer.work.component.js workComponent}
+ *  @requires     {@link break/timer.break.component.js breakComponent}
+ *  @description  Creates, registers and retrievs AngularJS module, controller, services, components and values
+ *  @author       Katarzyna Ziomek-Zdanowicz
+ *
+ */
 import angular from "angular";
 import AppController from "./app.controller";
 import AppTimerService from "./core/timer.service";
 import workComponent from "./work/timer.work.component";
 import breakComponent from "./break/timer.break.component";
-import workOptions from  "./work/timer.work.options";
-import breakOptions from  "./break/timer.break.options";
-import "./index.html";
+/**
+ * @typedef      TimerOptions
+ * @type         {object}
+ * @property     {string} id - an ID
+ */
+import workOptions from "./work/timer.work.options";
+import breakOptions from "./break/timer.break.options";
 
 export default angular
 .module("App", [])
@@ -15,5 +29,14 @@ export default angular
 .service("BreakService", ["$rootScope", "breakOptions", AppTimerService])
 .component("workComponent", workComponent)
 .component("breakComponent", breakComponent)
+
+/**
+ * @param {TimerOptions} workOptions
+ * @description register work options
+ */
 .value("workOptions", workOptions)
+/**
+* @param {TimerOptions} breakOptions
+* @description register break options
+*/
 .value("breakOptions", breakOptions);
