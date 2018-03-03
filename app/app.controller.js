8a7ec9b17e54c624d1ef8496f27561f9e7339b0a
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-03 10:45:05 +0100

    Comments changed (for JSdoc and custom template)

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,4 +1,9 @@
 /* jshint esversion: 6 */
+/**
+ * @controller app.controller
+ * @name app.controller
+ * @description app controller's function
+ */
 export default ($scope, $timeout, WorkService, BreakService) => {
   const me = $scope;
   const timeout = 2000;
